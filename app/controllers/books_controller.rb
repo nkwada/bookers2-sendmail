47eb936e384b96e8ed81ab9f48da7d8724f84c83
nkwada <n.qb.dft9@gmail.com>
2020-02-24 05:37:39 +0000

    [Fix]不具合の修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,26 @@
 class BooksController < ApplicationController
+before_action :correct_user, only: [:edit, :update]
+
 
   def show
   	@book = Book.find(params[:id])
+    @user = current_user
   end
 
   def index
   	@books = Book.all #一覧表示するためにBookモデルの情報を全てくださいのall
+    @book = Book.new
+    @user = current_user
   end
 
   def create
   	@book = Book.new(book_params) #Bookモデルのテーブルを使用しているのでbookコントローラで保存する。
+    @book.user_id = current_user.id
   	if @book.save #入力されたデータをdbに保存する。
   		redirect_to @book, notice: "successfully created book!"#保存された場合の移動先を指定。
   	else
   		@books = Book.all
+      @user = current_user
   		render 'index'
   	end
   end
@@ -33,16 +40,24 @@ class BooksController < ApplicationController
   	end
   end
 
-  def delete
+  def destroy
   	@book = Book.find(params[:id])
-  	@book.destoy
+  	@book.destroy
   	redirect_to books_path, notice: "successfully delete book!"
   end
 
   private
 
-  def book_params
-  	params.require(:book).permit(:title)
+    def book_params
+    	params.require(:book).permit(:title, :body)
+    end
+
+    def correct_user
+        book = Book.find(params[:id])
+        if current_user.id != book.user.id
+        redirect_to books_path
+    end
+
   end
 
 end
