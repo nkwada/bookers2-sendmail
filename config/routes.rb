608c6dbeb993d568061a888197571b4cfe03fd03
nkwada <n.qb.dft9@gmail.com>
2020-02-25 06:28:14 +0000

    [Edit]フォロー後のリダイレクト先の変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'home/about'
-  resources :books do
-  	resource :favorites, only: [:create, :destroy]
-  	resource :book_comments, only: [:create, :destroy]
-  end
  resources :users do
     member do
      get :following, :followers
     end
   end
   resources :relationships,       only: [:create, :destroy]
+  resources :books do
+  	resource :favorites, only: [:create, :destroy]
+  	resource :book_comments, only: [:create, :destroy]
+  end
+
   root 'home#top'
 end
