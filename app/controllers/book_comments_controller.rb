fee4e28069fc9cf66fc5c6f781f09d2e4f827d37
nkwada <n.qb.dft9@gmail.com>
2020-02-26 07:24:37 +0000

    [Edit]いいね・コメントの非同期通信の実装

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,19 +1,21 @@
 class BookCommentsController < ApplicationController
 	def create
-	    book = Book.find(params[:book_id])
-	    comment = current_user.book_comments.new(book_comment_params)
-	    comment.book_id = book.id
-	    if comment.save
-	    	redirect_back(fallback_location: root_path, notice: "successfully created comment!")
-		else
-			redirect_back(fallback_location: root_path)
-		end
+	    @book = Book.find(params[:book_id])
+	    @book_comment = current_user.book_comments.new(book_comment_params)
+	    @book_comment.book_id = @book.id
+	    @book_comment.save
+	    # if @book_comment.save
+	    	# render :index, notice: "successfully created comment!"
+		# else
+		# 	redirect_back(fallback_location: root_path)
+		# end
 	end
 
 	def destroy
-        @book_comment = BookComment.find(params[:book_id])
+		@book = Book.find(params[:book_id])
+        @book_comment = BookComment.find(params[:comment_id])
         @book_comment.destroy
-        redirect_back(fallback_location: root_path)
+        # render :index
 	end
 
 private
