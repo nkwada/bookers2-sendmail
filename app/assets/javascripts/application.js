2be8b777a759dbb3531c7cd789308e5e65a30679
nkwada <n.qb.dft9@gmail.com>
2020-03-14 15:13:29 +0000

    deploy setting

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,18 +19,6 @@
 //= require bootstrap-sprockets
 //= require_tree .
 
-$(function() {
-	$(document).on('turbolinks:load', () => {
-	   $('#user_postcode').jpostal({
-	     postcode: [ '#user_postcode' ],
-	     address:       {'#user_prefecture_code': '%3',
-	            	    '#user_address_city': '%4',
-	     		    '#user_address_street': '%5%6',
-	     		    '#user_address_building': '%7'
-	    }
-		});
-	});
-});
 
 
 
